23f089df0785edb29af8e54a70c4db5ddff43daa
vovanchubenko <112155197+vovanchubenko@users.noreply.github.com>
2023-02-23 16:53:18 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Telegraf = require('telegraf')
+const token = '6216248779:AAERKYlQDU-HD7ix6eIZJsN8xBgX4btUiKw'
+const bot = new Telegraf(token)
+
+// Обработчик команды /start
+bot.start((ctx) => {
+  ctx.reply('Привет! Этот бот поможет создать музыку в жанре phonk.')
+})
+
+// Обработчик команды /create_music
+bot.command('create_music', (ctx) => {
+  // Код для создания музыки в жанре phonk
+  // Используйте Tone.js или Web Audio API, чтобы создать музыку
+
+  // Загрузка готовой музыки на сервер Telegram и ее отправка пользователю
+  ctx.replyWithAudio({ source: 'path/to/music.mp3' })
+})
+
+// Обработчик команды /download_music
+bot.command('download_music', (ctx) => {
+  // Код для скачивания музыки на устройство пользователя
+  // Используйте fs и другие соответствующие модули Node.js для скачивания файла
+
+  // Отправка музыки пользователю
+  ctx.replyWithAudio({ source: 'path/to/music.mp3' })
+})
+
+// Запуск бота
+bot.launch()
